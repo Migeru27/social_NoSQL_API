e228ed7c4769b84fc4ee4a990aed6c5868d5b63b
Miguel A <migueldude27@gmail.com>
2023-04-28 06:42:52 -0400

    added files for server/ database

---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,41 @@
+//CRUD - Create, Read , Update, Delete
+const router = require('express').Router();
+
+// api/thoughts
+
+// GET all thoughts
+// CREATE a new thought
+const {
+    getOneThought,
+    getAllThoughts,
+    createThought,
+    deleteThought,
+} = require ('../../controllers/thoughtController')
+//Thoughts Model requires: thoughtID, thoughtText, username
+
+// GET ALL thoughts
+// CREATE a New thought
+router.route('/').get(getAllThoughts).post(createThought);
+// GET a specific thought
+// DELETE a specific thought
+router.route('/:_id').get(getOneThought).delete(deleteThought);
+// future-state UPDATE-POST an existing thought
+
+// CREATE a new reaction to another user's thought
+const {
+    getAllReactions,
+    getOneReaction,
+    createReaction,
+    deleteReaction,
+} = require ('../../controllers/usersReaction')
+
+//Reactions Model requires: reactionID as ObjectId, reactionBody as string & required and a username
+
+// For a single thought/User, get all reactions
+// For a single thought/User, get one specific thought
+// CREATE a new reaction to a user's thought
+// DELETE a specific reaction from a user's thought posting
+
+// future-state UPDATE-POST an existing reaction to a thought -- DELETE all reactions
+
+module.exports = router;
